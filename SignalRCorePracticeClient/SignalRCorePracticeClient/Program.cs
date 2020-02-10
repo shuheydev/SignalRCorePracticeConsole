5536f9e5ebd0ecd33599bf02d9b7b01246204784
shuhey <nishizawa@timedia.co.jp>
2020-02-10 14:12:32 +0900

    Initial commit

---
+++ SignalRCorePracticeClient/SignalRCorePracticeClient/Program.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SignalRCorePracticeClient
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            //HubConnectionオブジェクトを使ってサーバーのSignalRハブと通信する
+            HubConnection connection;
+
+            //SignalRハブとの通信を担うオブジェクトを生成
+            connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:44304/mysignalrhub")
+                .WithAutomaticReconnect()
+                .Build();
+
+            //クライアント側の受信時の処理を行うメソッドを登録する。
+            //文字列で指定した名前でサーバー側から呼び出される。
+            connection.On<string>("ReceiveMessage", ReceiveMessageClient);
+
+            try
+            {
+                //サーバー側のSignalRハブと接続
+                await connection.StartAsync();
+                Console.WriteLine("接続できました");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("接続できませんでした");
+            }
+
+            while (true)
+            {
+                Console.Write("メッセージを入力: ");
+
+                string input = Console.ReadLine();
+
+                try
+                {
+                    //メッセージ送信
+                    await connection.InvokeAsync<string>("SendToAllAsync", input);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("メッセージの送信に失敗しました");
+                }
+
+                Thread.Sleep(500);
+            }
+        }
+
+        /// <summary>
+        /// メッセージ受信時に実行される
+        /// </summary>
+        /// <param name="messageFromServer"></param>
+        private static void ReceiveMessageClient(string messageFromServer)
+        {
+            Console.WriteLine($"サーバーから{DateTimeOffset.Now.ToString("yyyy/MM/dd/HH:mm:ss.fff")}に受信:\n {messageFromServer}");
+        }
+    }
+}
---
+++ SignalRCorePracticeServer/SignalRCorePracticeServer/Hubs/MySignalRHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRCorePracticeServer.Hubs
+{
+    public class MySignalRHub : Hub
+    {
+        /// <summary>
+        /// クライアントから任意の個数のデータを受け取ることができる。今回は1個。
+        /// クライアントはこのメソッドを呼び出す。
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public async Task SendToAllAsync(string message)
+        {
+            //Client側のReceiveMessageメソッドを呼ぶ
+            //今回は受信したメッセージにタイムスタンプをつけてべてのクライアントに送信するだけ。
+            await Clients.All.SendAsync("ReceiveMessage", AppendTimeStamp(message));
+        }
+
+        private string AppendTimeStamp(string message)
+        {
+            return $"{message} : {DateTimeOffset.Now.ToString("yyyy/MM/dd/HH:mm:ss.fff")}";
+        }
+    }
+}
